374a0930e8dece80a73e9061a8140e0ff8df55a6
Michael Leon <fbmal7@meta.com>
2025-11-25 15:34:30 -0500

    replace leap with flash

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,7 +128,7 @@ require("lazy").setup({
           end,
           desc = "Goto Type Definition in new split window"
         },
-        { "gr",        function() Snacks.picker.lsp_references() end,        desc = "References",            nowait = true },
+        { "gr",        function() Snacks.picker.lsp_references() end,        desc = "References",           nowait = true },
         { "gs",        function() Snacks.picker.lsp_symbols() end,           desc = "LSP Symbols" },
         { "gS",        function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
         -- Scratch buffer
@@ -364,7 +364,19 @@ require("lazy").setup({
     },
 
     -- *** motion ***
-    'ggandor/leap.nvim',
+    {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {},
+      keys = {
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      },
+    },
 
     -- *** replacing ***
     'nvim-pack/nvim-spectre',
