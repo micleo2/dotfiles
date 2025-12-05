97e5dc828d52903dc073eb70ddcd48661d7c89f8
mike <programmermike1@gmail.com>
2025-12-05 00:17:30 -0800

    [install] Fixup install after actual usage

--- install/arch-hyprland.sh
+++ install/arch-hyprland.sh
@@ -6,9 +6,21 @@ sudo pacman -S waybar hyprpaper
 # install walker and providers
 paru -S walker-bin elephant-all-bin
 
+# Setup themes
+cd ~
+mkdir oss
+git clone https://github.com/basecamp/omarchy.git
+ln -sf ~/oss/omarchy/themes ~/.config/mythemes
+ln -sf ~/.config/mythemes/catppuccin ~/.config/mythemes/current
+
 # setup walker config files.
 ln -sf ~/dotfiles/.config/walker/config.toml ~/.config/walker/
 sudo mkdir -p /etc/xdg/walker/themes/omarchy-based/
 sudo ln -sf ~/.config/mythemes/current/ /etc/xdg/walker/themes/omarchy-based/
 sudo ln -sf ~/dotfiles/.config/walker/style.css /etc/xdg/walker/themes/omarchy-based/
 
+# setup hyprland config files.
+mkdir ~/.config/hypr/
+touch ~/.config/hypr/monitors.conf
+ln -sf ~/dotfiles/.config/hypr/hyprland.conf ~/.config/hypr
+ln -sf ~/dotfiles/.config/hypr/hyprpaper.conf ~/.config/hypr
--- install/arch-setup.sh
+++ install/arch-setup.sh
@@ -2,7 +2,7 @@
 
 # setup software from pacman
 sudo pacman -S --needed base-devel git
-sudo pacman -S base-devel git github-cli nvim fish unzip bat fd fzf rustup dolphin less noto-fonts-emoji otf-firamono-nerd nodejs npm
+sudo pacman -S base-devel git github-cli nvim fish unzip bat fd fzf rustup dolphin less noto-fonts noto-fonts-emoji otf-firamono-nerd nodejs npm
 
 # setup paru
 git clone https://aur.archlinux.org/paru.git
@@ -11,7 +11,7 @@ rustup default stable
 makepkg -si
 
 # download software from paru
-paru -S google-chrome kanata-bin
+paru -S google-chroje kanata-bin
 
 # setup github integration
 gh auth login
@@ -22,6 +22,12 @@ fisher install PatrickF1/fzf.fish
 
 # Setup dotfiles
 gh repo clone micleo2/dotfiles
+mkdir ~/.config/nvim
 ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish 
 ln -sf ~/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/
+ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty
+ln -sf ~/dotfiles/.config/kitty/current-theme.conf ~/.config/kitty
+
+# Kanata
+# Visit this link https://github.com/jtroo/kanata/wiki/Avoid-using-sudo-on-Linux
