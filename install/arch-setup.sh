77d82bdae9354fe906f67acac6d681983f557fec
michael <programmermike1@gmail.com>
2026-09-05 01:51:17 -0700

    [install] clean up

--- install/arch-setup.sh
+++ install/arch-setup.sh
@@ -3,13 +3,11 @@
 # setup software from pacman
 sudo pacman -S --needed base-devel git
 # (xorg-xhost needed by gparted and rpi-imager)
-sudo pacman -S --needed git github-cli os-prober man-db nvim fish unzip bat fd fzf rustup less hyprpolkitagent bitwarden nodejs npm zoxide gparted xorg-xhost
+sudo pacman -S --needed git github-cli os-prober man-db nvim nfs-utils fish unzip bat fd fzf rustup less bitwarden nodejs npm zoxide gparted xorg-xhost btop
 sudo pacman -S --needed noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji otf-firamono-nerd ttf-jetbrains-mono-nerd
-sudo pacman -S --needed pavucontrol sunshine syncthing ethtool ddcutil discord gwenview btop blueman
+sudo pacman -S --needed pavucontrol syncthing discord gwenview
 sudo pacman -S --needed pavucontrol ethtool ddcutil
-
-# yazi
-sudo pacman -S --needed yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+sudo pacman -S --needed pavucontrol moonlight-qt
 
 sudo pacman -S mesa
 
@@ -36,25 +34,22 @@ makepkg -si
 # For a high res monitor, set the following in grub
 # GRUB_GFXMODE=1920x1080x32
 
-# download software from paru
-paru -S google-chrome kanata-bin
-paru -S localsend
-
 # setup github integration
 gh auth login
 
 # install fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install PatrickF1/fzf.fish
+sudo pacman -S --needed fisher &&
+  fisher install PatrickF1/fzf.fish && fisher install kidonng/zoxide.fish
 
 # Setup dotfiles
 gh repo clone micleo2/dotfiles
 mkdir ~/.config/nvim
-ln -s ~/dotfiles/.config/lazynvim ~/.config/nvim
-ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish 
-ln -s ~/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/
-ln -s ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty
-ln -s ~/dotfiles/.config/kitty/current-theme.conf ~/.config/kitty
+ln -s ~/dotfiles/.config/lazynvim ~/.config/nvim &&
+  ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish &&
+  ln -s ~/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/ &&
+  ln -s ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty &&
+  ln -s ~/dotfiles/.config/kitty/current-theme.conf ~/.config/kitty &&
 
-# Kanata
-# Visit this link https://github.com/jtroo/kanata/wiki/Avoid-using-sudo-on-Linux
+  # Kanata
+  # Visit this link https://github.com/jtroo/kanata/wiki/Avoid-using-sudo-on-Linux
+  paru -S kanata-bin
